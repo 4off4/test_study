663a1f5580dbbad84c5d22b994cc70e4df8037b0
eun hee <4off4@naver.com>
2025-08-20 14:10:00 +0900

    [프로그래머스 Lv.0] A강조하기

---
+++ src/A강조하기.java
@@ -0,0 +1,8 @@
+public class A강조하기 {
+    public static String solution(String myString) {
+        return myString.toLowerCase().replace("a", "A");
+    }
+    public static void main(String[] args) {
+        System.out.println(solution("abstract algebra"));
+    }
+}
