687dc4cf4a5e32acd15afd150446066552125bf9
eun hee <4off4@naver.com>
2025-10-20 17:47:44 +0900

    [정보처리기사] JAVA 실기 (2025년 완)

---
+++ src/t15.java
@@ -0,0 +1,34 @@
+public class t15 {
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println(calc("5"));
+        }
+
+        static int calc(int value) {
+            if (value <= 1) return value;
+            return calc(value - 1) + calc(value - 2);
+        }
+
+        static int calc(String str) {
+            int value = Integer.valueOf(str);
+            if (value <= 1) return value;
+            return calc(value - 1) + calc(value - 3);
+        }
+    }
+}
+
+/*
+    STEP 1. calc(4) + calc(3)
+
+    STEP 2. calc(4)
+    - calc(3) + calc(2)
+    -> calc(3) => 1
+    -> calc(2) => 1
+    => calc = 2
+
+    STEP 3. calc(3)
+    - calc(2) + 1
+    - 1 + 1  = 2
+
+    결국 4
+*/
